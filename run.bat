8fb47b6d1fcececf8dd8f0a1e7b447206c624503
Dominik93 <slusarz.dominik@gmail.com>
2020-01-19 09:12:13 +0100

    add run script and repackage for spring module

---
+++ run.bat
@@ -0,0 +1,9 @@
+start java -jar work-support-eureka-server\target\work-support-eureka-server-1.0.0-SNAPSHOT.jar
+start java -jar work-support-zuul-apigateway\target\work-support-zuul-apigateway-1.0.0-SNAPSHOT.jar
+
+start java -jar work-support-module\work-support-module-context\target\work-support-module-context-1.0.0-SNAPSHOT.jar
+start java -jar work-support-module\work-support-module-sql-executor\target\work-support-module-sql-executor-1.0.0-SNAPSHOT.jar
+start java -jar work-support-module\work-support-module-script-executor\target\work-support-module-script-executor-1.0.0-SNAPSHOT.jar
+start java -jar work-support-module\work-support-module-log-downloader\target\work-support-module-log-downloader-1.0.0-SNAPSHOT.jar
+
+start java -jar work-support-web\work-support-module-host\target\work-support-module-host-1.0.0-SNAPSHOT.jar
